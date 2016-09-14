87f265b52071036ec5d4af1305744eddea1db06a
asa52 <asa52@cam.ac.uk>
2016-09-14 09:54:04 +0100

    Revert "Revert "HillWalk 3""
    This reverts commit d2a0882d16bc1103dce9f616ebbc858dfdbbbf76.

--- laser_align/baking.py
+++ laser_align/baking.py
@@ -157,7 +157,7 @@ def revisit_check(results, proposed_pos, overlap_allowed):
     measured points."""
     proposed_pos = set(tuple(row) for row in proposed_pos)
 
-    if (not results) or overlap_allowed:
+    if not list(results) or overlap_allowed:
         # If overlap is allowed, then all proposed positions will be visited.
         visited_pos = set()
     else:
@@ -197,11 +197,7 @@ def to_parmax(results_arr, scope_obj, axis, move=True):
     x = results_arr[:, ['x', 'y', 'z'].index(axis) + 1]
     x_range = (np.min(x), np.max(x))
     y = results_arr[:, 4]
-
-    assert np.where(y == np.max(y)) != 0 and \
-        np.where(y == np.max(y)) != len(y), 'Extrapolation occurred - ' \
-                                            'measure over a wider range.'
-
+    print x, y
     reg = np.polyfit(x, y, 2, full=True)
     coeffs = reg[0]
     residuals = reg[1]
@@ -209,13 +205,18 @@ def to_parmax(results_arr, scope_obj, axis, move=True):
     coeffs_deriv = np.array([2, 1]) * coeffs[:2]    # Simulate differentiation.
     x_stat = -coeffs_deriv[1] / coeffs_deriv[0]
     pred_y = coeffs[0]*x_stat**2 + coeffs[1]*x_stat + coeffs[2]
+
     new_pos = results_arr[0, 1:4]     # Get the values that stay the same.
     new_pos[['x', 'y', 'z'].index(axis)] = x_stat   # Overlay with max.
     print "new pos parmax", new_pos
 
     if move:
         scope_obj.stage.move_to_pos(new_pos)
-        return
+        return new_pos, pred_y, x_range, residuals, coeffs
+    elif coeffs[0] > 0:
+        # Requirement for maximum point, otherwise move to measured
+        # maximum position.
+        return False
     else:
         return new_pos, pred_y, x_range, residuals, coeffs
 
